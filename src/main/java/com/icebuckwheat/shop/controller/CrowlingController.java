c6ace3f0c8f8088e30bc63fdafe475a09fb3c0c3
jangwoolee <8282qwe@gmail.com>
2024-08-29 20:38:35 +0900

    finish!!!!!!!!!!!!!!

--- src/main/java/com/icebuckwheat/shop/controller/CrowlingController.java
+++ src/main/java/com/icebuckwheat/shop/controller/CrowlingController.java
@@ -4,7 +4,6 @@ import com.icebuckwheat.shop.dto.ItemDto;
 import com.icebuckwheat.shop.service.Crowling;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +14,7 @@ import java.util.ArrayList;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/shop")
-@CrossOrigin(value = "http://localhost:8080",allowCredentials ="true")
+//@CrossOrigin(value = "http://localhost:8080",allowCredentials ="true")
 public class CrowlingController {
 
     private final Crowling crowling;
