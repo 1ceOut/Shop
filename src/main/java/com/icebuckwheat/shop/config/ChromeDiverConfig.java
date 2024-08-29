3f3291d4d88453763a98758ca4aefdf5502d1b0b
jangwoolee <8282qwe@gmail.com>
2024-08-29 20:55:05 +0900

    finish!!!!!!!!!!!!!!

--- src/main/java/com/icebuckwheat/shop/config/ChromeDiverConfig.java
+++ src/main/java/com/icebuckwheat/shop/config/ChromeDiverConfig.java
@@ -10,7 +10,7 @@ public class ChromeDiverConfig {
     @Bean
     public ChromeOptions ChromeOptions() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
+        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
